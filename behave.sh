27f34398fd3ddbc22dcc3ff30906f10f9b9e715f
Colin Moore-Hill <colin.moore-hill@met.police.uk>
2016-02-19 00:25:37 +0000

    modified:   behave.sh

--- behave.sh
+++ behave.sh
@@ -1,4 +1,8 @@
-source pyEnvJenkins/bin/activate
-pip install -r requirements.txt
+#!/bin/bash
+if [ ! -d "pyEnvJenkins" ]; then
+        virtualenv pyEnvJenkins
+fi
+. pyEnvJenkins/bin/activate
+pip install -r requirements.txt 
 cd BrowserStackBehave/
 behave
